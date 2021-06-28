bcd8e2ec0c144c38d7e66abac29e1a367c476d67
piyanut.rk <piyanut.rk@gmail.com>
2021-06-28 17:31:49 +0700

    Add project files.

---
+++ DGV select sum/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace DGV_select_sum
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbCount = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbSum = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 130);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(877, 406);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(113, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Count :";
+            // 
+            // lbCount
+            // 
+            this.lbCount.AutoSize = true;
+            this.lbCount.Location = new System.Drawing.Point(160, 48);
+            this.lbCount.Name = "lbCount";
+            this.lbCount.Size = new System.Drawing.Size(10, 13);
+            this.lbCount.TabIndex = 1;
+            this.lbCount.Text = "-";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(261, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Sum:";
+            // 
+            // lbSum
+            // 
+            this.lbSum.AutoSize = true;
+            this.lbSum.Location = new System.Drawing.Point(298, 48);
+            this.lbSum.Name = "lbSum";
+            this.lbSum.Size = new System.Drawing.Size(10, 13);
+            this.lbSum.TabIndex = 1;
+            this.lbSum.Text = "-";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(877, 536);
+            this.Controls.Add(this.lbSum);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbCount);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbCount;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbSum;
+    }
+}
+
---
+++ DGV select sum/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DGV_select_sum
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.ColumnCount = 2;
+            dataGridView1.Columns[0].Name = "Product ID";
+            dataGridView1.Columns[1].Name = "Product Name";
+            for (int i = 1; i <= 10; i++)
+            {
+                dataGridView1.Rows.Add(i.ToString(),"abc");
+                dataGridView1.Rows.Add(null,"null");
+
+            }
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            var dgv = sender as DataGridView;
+            lbCount.Text = dgv.SelectedCells.Count.ToString();
+            decimal sum = 0;
+            decimal temp = 0;
+            foreach (DataGridViewCell item in dgv.SelectedCells)
+            {
+                if (item.Value != null)
+                {
+                    if (decimal.TryParse(item.Value.ToString(),out temp))
+                    {
+                        sum += temp;
+                    }
+                }
+            }
+
+            lbSum.Text = sum.ToString();
+            //for (int i = 0; i < dataGridView1.SelectedCells.Count; i++)
+            //{
+            //    if (true)
+            //    {
+
+            //    }
+            //}
+        }
+    }
+}
